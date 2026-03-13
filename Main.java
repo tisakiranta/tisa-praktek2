d4f3fb4ddaaaa5c5824f4a5f8ac7ce726836263c
tisakiranta <tisakiranta@gmail.com>
2026-03-12 20:25:28 -1200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Weapon tombak = new Weapon();
+        tombak.name = "Tombak Trisula";
+        tombak.damage = 160;
+        tombak.showWeapon();
+    }
+}
---
+++ Weapon.java
@@ -0,0 +1,12 @@
+public class Weapon {
+   String name;
+   int damage;
+
+   public Weapon() {
+   }
+
+   public void showWeapon() {
+      System.out.println("Nama Senjata : " + this.name);
+      System.out.println("Damage       : " + this.damage);
+   }
+}
